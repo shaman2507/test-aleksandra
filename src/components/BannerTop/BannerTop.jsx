83c1bb3b99e35f18d5f7ba7c3014029b157cc7e1
shaman2507 <alabay174@gmail.com>
2024-01-11 18:14:46 +0100

    banner top tablet

--- src/components/BannerTop/BannerTop.jsx
+++ src/components/BannerTop/BannerTop.jsx
@@ -1,27 +1,26 @@
 import css from './BannerTop.module.css';
-import bannerTopMobile from '../../img/bannerTopMobile.jpg';
-import menu from '../../img/menu-icon.svg';
 
 const BannerTop = () => {
     return (
         <div className={css.bannerTopContainer}>
             <div>
-                <img
-                    className={css.bannerTopImg}
-                    src={bannerTopMobile}
-                    alt='Banner top mobile'
-                    width="128"
-                    height="54" />
+                <img className={css.bannerTopImg} alt='Banner top' />
             </div>
             <div className={css.bannerTopContent}>
                 <p className={css.bannerTopTitle}>
-                    Black Friday,
-                    <span className={css.bannerTopText}><b>10%OFF</b></span>
+                    <b>Black Friday,</b>
+                </p>
+                <p className={css.bannerTopText}>
+                    <b>10%OFF</b>
+                </p>
+                <p className={css.bannerTopTextFr}>
+                    Use code <span className={css.bannerTopTextFrSpn}><b>10FRIDAY</b></span>
                 </p>
             </div>
             <div className={css.bannerMenu}>
                 <button type="button" className={css.bannerTopBtn}>
-                    <img src={menu} alt='Menu button' height="30" weight="30"></img>
+                    <img className={css.bannerTopBtnImg} alt='Menu button' ></img>
+                    <p className={css.bannerTopBtnText}>Shop now</p>
                 </button>
             </div>
         </div>
