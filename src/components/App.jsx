64cb0bcd6aa8c7993a653fa0dec291d78ee591a0
shaman2507 <alabay174@gmail.com>
2024-01-12 15:18:58 +0100

    banner bottom appearance and closing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
+import "./App.css";
 import BannerTop from "./BannerTop/BannerTop";
 import BannerBottom from "./BannerBottom/BannerBottom";
 
+
 export const App = () => {
+  const [showBannerBottom, setshowBannerBottom] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const triggerDistance = 150;
+      const isScrollPastTrigger = window.scrollY > triggerDistance;
+      setshowBannerBottom(isScrollPastTrigger);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <BannerTop />
-      <BannerBottom />
+      {showBannerBottom && <BannerBottom />}
     </>
   );
 };
--- src/components/BannerBottom/BannerBottom.jsx
+++ src/components/BannerBottom/BannerBottom.jsx
@@ -1,22 +1,41 @@
+import { useState } from 'react';
 import css from './BannerBottom.module.css';
 
 const BannerBottom = () => {
+    const [isBannerClosed, setIsBannerClosed] = useState(
+        sessionStorage.getItem("isBannerClosed") === "true"
+    );
+
+    const handleBannerClose = () => {
+        sessionStorage.setItem("isBannerClosed", "true");
+        setIsBannerClosed(true);
+    };
+
+    if (isBannerClosed) {
+        return null;
+    }
+
     return (
-        <div className={css.bannerBtmContainer}>
-            <button className={css.bannerBtmCloseBtn} type="button">
-                <img className={css.bannerBtmCloseBtnImg} alt='Menu button' ></img>
-            </button>
-            <div className={css.bannerBtmContent}>
-                <h2 className={css.bannerBtmTitle}>Black Friday</h2>
-                <h3 className={css.bannerBtmSubtitle}>10%OFF</h3>
-                <p className={css.bannerBtmText}>
-                    Use code <span className={css.bannerBtmPart}>10FRIDAY</span> at checkout
-                </p>
-                <button className={css.bannerBtmBtn} type="button" >
-                    <p className={css.bannerBtmBtnText}>Shop now</p>
-                    </button>
+        <>
+            <div className={css.bannerBtmContainer}>
+                <button className={css.bannerBtmCloseBtn} type="button" onClick={handleBannerClose}>
+                    <img className={css.bannerBtmCloseBtnImg} alt='Menu button' ></img>
+                </button>
+                <div className={css.bannerBtmWrapper}>
+                    <div className={css.bannerBtmContent}>
+                        <h2 className={css.bannerBtmTitle}>Black Friday</h2>
+                        <h3 className={css.bannerBtmSubtitle}>10%OFF</h3>
+                        <p className={css.bannerBtmText}>
+                            Use code <span className={css.bannerBtmPart}>10FRIDAY</span> at checkout
+                        </p>
+                        <button className={css.bannerBtmBtn} type="button" >
+                            <p className={css.bannerBtmBtnText}>Shop now</p>
+                            <p className={css.bannerBtmBtnTextFull}>Shop now through Monday</p>
+                        </button>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
